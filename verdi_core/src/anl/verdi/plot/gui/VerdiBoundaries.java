10e6124860e43fb945a4c7a64417e21bebcdfb49
lizadams <lizadams@email.unc.edu>
2015-09-09 12:51:06 -0400

    Changes to code to remove bin files and replace with shapefiles for background maps.
    Former-commit-id: 317476300aebab9e85ac0995950d7b7197e62942

---
+++ verdi_core/src/anl/verdi/plot/gui/VerdiBoundaries.java
@@ -0,0 +1,235 @@
+/**
+ * VerdiBoundaries.java
+ * Read a shapefile, clip it to a raster (grid) file, clip a raster (grid) file to this shapefile
+ * Replaces old gov.epa.emvl.MapLines.java, which provided similar functionality for old .BIN files
+ * 
+ * @author Jo Ellen Brandmeyer, Institute for the Environment, 2015
+ *
+ */
+
+package anl.verdi.plot.gui;
+
+import java.awt.Graphics;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.map.MapContent;
+import org.geotools.referencing.CRS;
+import org.geotools.styling.Style;
+import org.opengis.feature.simple.SimpleFeature;
+//import org.opengis.geometry.Geometry;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+
+import com.vividsolutions.jts.geom.Geometry;
+// VerdiBoundaries provides VerdiStyle and CRS for a shapefile (e.g., state boundaries)
+// & transforms it to the CRS of a grid file
+// independent of time & vertical layer
+
+public class VerdiBoundaries {
+
+	static final Logger Logger = LogManager.getLogger(VerdiBoundaries.class.getName());
+	private VerdiStyle aVerdiStyle = null;
+	private String vFileName = null;		// name of shapefile
+	private File vFile = null;				// shapefile represented as a File
+	private String vPath = null;			// vPath is absolute path to Shapefile
+	private FileDataStore vStore = null;	// vStore is the FileDataStore associated with the vFile
+	private FeatureSource vFeatureSource = null;
+	private MapContent vMap = null;			// 
+	private MathTransform vTransform = null;	// math transform from Shapefile to grid CRS
+	
+	public VerdiBoundaries()		// default constructor
+	{
+		reset();
+		Logger.debug("done with default constructor for VerdiBoundaries");	// JEB done MANY MANY times - part of infinite loop ???
+	}
+	
+	public boolean setFileName(String aFileName)	// set values from a file name belonging to a shapefile
+	{
+		Logger.debug("setting file name in VerdiBoundaries to: " + aFileName);		// JEB OK done once per std shapefile (7 times)
+		if(aFileName == null)		// cannot pass in a null for the file name
+			return false;
+		if(vFileName != null)		// already had one, so reset before storing this new one
+			reset();
+		vFileName = new String(aFileName);
+									// looking for "shp" at end of file name to designate a Shapefile
+		String last3Chars = new String(vFileName.substring(vFileName.length() - 3, vFileName.length()));
+		Logger.debug("fileExtension = " + last3Chars);		// JEB OK done 7 times
+		
+		// check that file name has extension "shp" ignorecase
+		// if not, reset() and return false;
+		if(last3Chars.compareToIgnoreCase("shp") != 0)
+		{
+			Logger.error("File name must have extension shp - select a different file.");
+			System.err.println("File name must have extension shp - select a different file.");
+			reset();
+			return false;
+		}
+		
+		// here check that the file exists
+		// if not, reset() and return false;
+		
+		vFile = new File(vFileName);			// get File object for this file name
+		if(!vFile.exists() || !vFile.canRead() || vFile.isDirectory())
+		{
+			reset();
+			Logger.error("File " + vFileName + " does not exist, cannot be read, or is a directory");
+			System.err.println("File " + vFileName + " does not exist, cannot be read, or is a directory");
+			return false;
+		}
+		vPath = vFile.getAbsolutePath();		// definition taken from FastTileAddLayerWizard
+		vMap = new MapContent();	// THINK THROUGH THIS SOME MORE - JEB
+		aVerdiStyle = new VerdiStyle(vFile);	// read/compute everything to make a VerdiStyle for this File
+		Logger.debug("Successfully completed VerdiBoundaries.setFileName for: " + vPath +
+				" and back from instantiating VerdiStyle.");	// JEB BACK FROM CREATING new VerdiStyle
+		return true;	// successfully set data members		// JEB YES returning to calling pgm (Mapper)
+	}
+	
+	private void reset()	// reset member variables to null
+	{
+		aVerdiStyle = null;
+		vFileName = null;
+		vFile = null;
+		vPath = null;
+		vMap = null;
+	}
+	
+	public void draw(double[][] domain, double[][] gridBounds, CoordinateReferenceSystem gridCRS, Graphics graphics,
+			int xOffset, int yOffset, int width, int height)	// execute the draw function for this VerdiBoundaries layer
+	{	
+		Style theStyle = aVerdiStyle.getStyle();
+		Logger.debug("in VerdiBoundaries.draw; aVerdiStyle.getStyle() = " + theStyle.toString());
+		Logger.debug("in VerdiBoundaries.draw, CRS for tile plot (gridCRS) = " + gridCRS);
+		Logger.debug("   and name of file = " + vFileName);
+		// here write out stored values for checking
+//		for (double aDbl[] : domain)
+//		{
+//			for (double bDbl : aDbl)
+//			{
+//				Logger.debug("domain = " + bDbl); // in degrees: lonMin, lonMax, latMin, latMax
+//			}
+//		}
+//
+//		for (double cDbl[] : gridBounds)
+//		{
+//			for (double dDbl : cDbl)
+//			{
+//				Logger.debug("gridBounds = " + dDbl); // in meters -2628000, 2592000, -1728000, 1944000
+//			}
+//		}
+		try {
+			vStore = FileDataStoreFinder.getDataStore(vFile);
+			vFeatureSource = vStore.getFeatureSource();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		Logger.debug("vFile = " + vFile.toString());
+		Logger.debug("vPath = " + vPath.toString());
+		Logger.debug("vMap = " + vMap.toString());
+		Logger.debug("map Coordinate Reference System (vCRS) = " + aVerdiStyle.getCoordinateReferenceSystem().toString());
+		Logger.debug("Feature Source = " + aVerdiStyle.getFeatureSource().toString());
+		Logger.debug("vStore = " + vStore.toString());
+		Logger.debug("vFeatureSource = " + vFeatureSource.toString());
+		Logger.debug("Layer = " + aVerdiStyle.getLayer().toString());
+		Logger.debug("ShapeFile = " + aVerdiStyle.getShapeFile().toString());
+		Logger.debug("Shapefile Path = " + aVerdiStyle.getShapePath());
+		Logger.debug("Style = " + aVerdiStyle.getStyle().toString());
+		Logger.debug("now set the CRS to gridCRS");	// NOTE: do NOT use Projector method (old UCAR operates point-by-point
+		vMap.getViewport().setCoordinateReferenceSystem(gridCRS);
+		Logger.debug("set CRS of Viewport to gridCRS");
+		aVerdiStyle.getLayer().setVisible(true);
+		// show(); perhaps try show instead of setVisible ??? JEB
+		
+		// set of math transform
+		// NOTE: next commented-out section is possibly the beginnings of writing out a shapefile in a given projection
+//		boolean lenient = true;		// allow for some error due to different datums
+//		try {
+//			vTransform = CRS.findMathTransform(aVerdiStyle.getCoordinateReferenceSystem(), gridCRS, lenient);
+//		} catch (FactoryException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		Logger.debug("vTransform computed as = " + vTransform.toString());
+//		// TODO - perform the transform
+//		SimpleFeatureCollection featureCollection = null;
+//		SimpleFeatureCollection newFeatureCollection = null;
+//		try {
+//			featureCollection = (SimpleFeatureCollection) vFeatureSource.getFeatures();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		SimpleFeatureIterator iterator = featureCollection.features();
+//		try {
+//			while (iterator.hasNext())
+//			{
+//				SimpleFeature feature = iterator.next();
+//				Geometry geom1 = (Geometry) feature.getDefaultGeometry();
+//				Geometry geometry2 = JTS.transform(geom1, vTransform);
+//				newFeatureCollection.
+//				// What do I do with the geometry2 object?
+//			}
+//		} catch (Exception ex) {
+//			Logger.error("caught an exception while converting map to new Coordinate Reference System");
+//		} finally {
+//			iterator.close();
+//		}
+		// TODO - create a map layer and add it to the vMap object
+		//vMap.layers().add(aVerdiStyle.getLayer());	// FIGURE THIS ONE OUT BECAUSE TRANSFORM IS HERE
+	}	// end of draw function
+	
+	public MapContent getMap()	// send vMap back to calling program
+	{
+		return vMap;
+	}
+
+	public boolean clipShapefileToGrid()	// clip extent of shapefile to match spatial grid (raster)
+	{	// TODO
+		return true;
+	}
+	
+	public boolean clipGridToShapefile()	// clip extent of the spatial grid (raster) to match shapefile
+	{	// TODO
+		return true;
+	}
+
+	public String getFileName() {
+		return vFileName;
+	}
+	
+	public File getFile() {
+		return vFile;
+	}
+	
+	public void setPath(String aPath)
+	{
+		vPath = aPath;
+	}
+	
+	public String getPath()
+	{
+		return vPath;
+	}
+	
+	public VerdiStyle getVerdiStyle()
+	{
+		return aVerdiStyle;
+	}
+	
+	public CoordinateReferenceSystem getCRS()
+	{
+		if(aVerdiStyle == null)
+			return null;
+		return aVerdiStyle.getCoordinateReferenceSystem();	// get the Coordinate Reference System for this File
+	}
+}
---
+++ verdi_core/src/anl/verdi/plot/gui/VerdiStyle.java
@@ -0,0 +1,323 @@
+/**
+ * VerdiStyle.java
+ * Get a Style for a map file in shapefile format
+ * Already have a shapefile. Pass its File object as an argument into the VerdiStyle constructor.
+ * Parts of this class definition are adapted from the StyleLab tutorial of GeoTools v12.
+ * NOTE: No member function is provided for writing out an SLD file.
+ */
+package anl.verdi.plot.gui;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;		// using log4j instead of System.out.println for messages
+import org.apache.logging.log4j.Logger;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.styling.FeatureTypeStyle;
+import org.geotools.styling.Fill;
+import org.geotools.styling.Graphic;
+import org.geotools.styling.LineSymbolizer;
+import org.geotools.styling.Mark;
+import org.geotools.styling.PointSymbolizer;
+import org.geotools.styling.PolygonSymbolizer;
+import org.geotools.styling.Rule;
+import org.geotools.styling.SLDParser;
+import org.geotools.styling.Stroke;
+import org.geotools.styling.Style;
+import org.geotools.styling.StyleFactory;
+import org.geotools.swing.styling.JSimpleStyleDialog;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.FilterFactory;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Polygon;
+/**
+ * @author Jo Ellen Brandmeyer, Institute for the Environment, 2015
+ *
+ */
+public class VerdiStyle {
+
+	private StyleFactory styleFactory = CommonFactoryFinder.getStyleFactory(null);
+	private FilterFactory filterFactory = CommonFactoryFinder.getFilterFactory(null);
+	static final Logger Logger = LogManager.getLogger(VerdiStyle.class.getName());
+	private Style vStyle = null;		// Style associated with this Shapefile
+	private FeatureSource vFeatureSource = null;
+	private File vFile = null;			// File associated with this Shapefile
+	private String shpPath = null;		// path (as a String) to this Shapefile
+	private Layer vLayer = null;
+	private CoordinateReferenceSystem vCRS = null;	// CRS for this Shapefile
+	private FileDataStore vStore = null;	// this is used despite what Eclipse says!
+
+	public VerdiStyle(File shpFile)		// File must previously exist and be for a .shp file
+	{									// constructor called from VerdiBoundaries.java
+		Logger.debug("in constructor of VerdiStyle with passed File");	// JEB YES
+		if(shpFile == null)
+		{
+			Logger.error("No shapefile defined for map layer");
+			return;
+		}
+		Logger.debug("have absolute path for shpFile = " + shpFile.getAbsolutePath());	// JEB YES
+		if(vFile != null)
+			reset();			// already had file set so must reset everything
+		vFile = shpFile;		// store shapefile into class data member
+		Logger.debug("stored shpFile as vFile = " + vFile.toString());	// JEB YES
+		shpPath = vFile.getAbsolutePath();
+		Logger.debug("shpPath set to: " + shpPath);	// JEB YES
+		calcVerdiStyle();			// JEB YES
+	}			// JEB YES returning to calling pgm (VerdiBoundaries)
+	
+	public VerdiStyle()		// default constructor
+	{
+		Logger.debug("in default constructor for VerdiStyle");
+		reset();		// no shapefile provided so create object with null data members
+	}
+	
+	public boolean setShapefile(File shpFile)	// assigns value to vFile and shpPath based on the file parameter
+	{	// returns true == set shapefile and path, false == reset data members only
+		if(shpFile == null)
+		{
+			reset();
+			return false;
+		}
+		vFile = shpFile;
+		shpPath = vFile.getAbsolutePath();
+		return true;
+	}
+	
+	public void calcVerdiStyle()		// get Style and CRS for a shapefile
+	{
+		Logger.debug("starting calcVerdiStyle");		// JEB YES
+		vFeatureSource = findFeatureSource();	// gets the FeatureSource for the shapefile
+		Logger.debug("vFeatureSource = " + vFeatureSource.toString());		// JEB YES (back from findFeatureSource()
+		if (vFeatureSource == null)
+			Logger.debug("vFeatureSource is null");
+		else
+		{
+			vStyle = createStyle();			// JEB YES
+			Logger.debug("vStyle = " + vStyle.toString());		// JEB YES StyleImpl[ name=Default Styler]
+			vCRS = vFeatureSource.getSchema().getCoordinateReferenceSystem();
+			Logger.debug("vCRS = " + vCRS.toString());			// JEB YES GEOGCS["SPHERE",
+		}
+	}
+
+	private void reset()	// reset member variables to null
+	{
+		vFile = null;
+		vFeatureSource = null;
+		shpPath = null;
+		vStyle = null;
+		vCRS = null;
+		vLayer = null;
+		vStore = null;
+	}
+
+	private FeatureSource findFeatureSource()	// find the FeatureSource for the shapefile
+	{
+		vFeatureSource = null;
+		try{
+			FileDataStore vStore = FileDataStoreFinder.getDataStore(vFile);
+			Logger.debug("got FileDataStore = " + vStore.toString());		// JEB YES
+			vFeatureSource = vStore.getFeatureSource();
+			Logger.debug("got vFeatureSource = " + vFeatureSource.toString());	// JEB YES
+		} catch (IOException ioEx) {
+			Logger.error("Data store or feature source for file " + vFile + " could not be found.");
+			Logger.error(ioEx.getMessage());
+		}
+		return vFeatureSource;
+	}
+
+	private Style createStyle()	// create the style from the SLD file, type of geometry, or dialog box
+	{
+		Logger.debug("in createStyle; ready to create Style vStyle as null");		// JEB YES
+		vStyle = null;
+		File sld = toSLDFile();			// JEB YES, there & returned with a null
+		if(sld != null)
+		{
+			Logger.debug("sld != null: " + sld.getAbsolutePath());
+			vStyle = createFromSLD(sld);
+			Logger.debug("back from createFromSLD");
+			if(vStyle != null)
+				return vStyle;
+		}
+		// sld == null: style could not be generated from .SLD file
+		Logger.debug("sld could not be generated from .SLD file, trying from pgm");		// JEB YES
+		vStyle = getStyleFromPgm(vFeatureSource);
+		if(vStyle != null)
+		{
+			Logger.debug("got vStyle, returning: " + vStyle.toString());		// JEB YES StyleImp[ name=Default Styler]
+			return vStyle;
+		}
+		// sld == null: style could not be programmatically generated
+		// last resort: create style via dialog box for user
+		Logger.debug("still no sld, trying getStyleFromDialog for vFeatureSource = " + vFeatureSource.toString());
+		vStyle = getStyleFromDialog(vFeatureSource);
+		Logger.debug("returning vStyle = " + vStyle.toString());
+		return vStyle;
+	}
+
+	public Style getStyleFromDialog(FeatureSource aFeatureSource)
+	{		// display dialog box for user to interactively specify style attributes (not available in script mode)
+		SimpleFeatureType schema = (SimpleFeatureType)aFeatureSource.getSchema();
+		Logger.debug("in getStyleFromDialog; SimpleFeatureType schema = " + schema.toString());
+		return JSimpleStyleDialog.showDialog(null, schema);
+	}
+
+	public Style getStyleFromPgm(FeatureSource aFeatureSource)
+	{		// get style based on type of geometry in the File
+		SimpleFeatureType schema = (SimpleFeatureType)aFeatureSource.getSchema();
+		Logger.debug("in getStyleFromPgm; SimpleFeatureType schema = " + schema.toString());		// JEB YES knows map_world and MultiLineString
+		Class geomType = schema.getGeometryDescriptor().getType().getBinding();
+		Logger.debug("geomType = " + geomType.toString());		// JEB YES class com.vividsolutions.jts.geom.MultiLineString
+
+		if(Polygon.class.isAssignableFrom(geomType) || MultiPolygon.class.isAssignableFrom(geomType))
+		{
+			Logger.debug("returning PolygonStyle");
+			return createPolygonStyle();
+		}
+		else if (LineString.class.isAssignableFrom(geomType) || MultiLineString.class.isAssignableFrom(geomType))
+		{
+			Logger.debug("returning LineStyle");		// JEB YES for map_world
+			return createLineStyle();			// JEB YES
+		}
+		else
+		{
+			Logger.debug("returning PointStyle");
+			return createPointStyle();
+		}
+	}
+
+	private File toSLDFile()		// get existing SLD file
+	{
+		Logger.debug("looking for existing SLD file");			// JEB YES from createStyle
+		String base = shpPath.substring(0, shpPath.length()-4);
+		String sldPath = base + ".sld";		// look for file with lower-case extension
+		File sldFile = new File(sldPath);
+		if(sldFile.exists())
+			return sldFile;
+
+		sldPath = base + ".SLD";			// look for file with upper-case extension
+		sldFile = new File(sldPath);
+		if(sldFile.exists())
+			return sldFile;
+		Logger.debug("Did not find existing SLD file; returning null");		// JEB YES
+		return null;						// did not find the .sld file
+	}
+
+	private Style createFromSLD(File sld)		// create a Style from the definition in an SLD file
+	{
+		Logger.debug("Trying to create a style from the SLD file " + sld.toString());
+		try{
+			SLDParser styleReader = new SLDParser(styleFactory, sld.toURI().toURL());
+			Style[] aStyle = styleReader.readXML();
+			Logger.debug("sending back " + aStyle[0].toString());
+			return aStyle[0];
+		} catch(Exception e) {
+			Logger.error("Unable to create a Style from the SLD file associated with :" + vFile);
+			return null;
+		}
+	}
+	
+	private Style createPolygonStyle()	// create a style to draw polygons
+	{
+		// create an opaque outline stroke
+		Stroke stroke = styleFactory.createStroke(
+				filterFactory.literal(Color.black), 
+				filterFactory.literal(1), 
+				filterFactory.literal(1));
+		// create a transparent fill
+		Fill fill = styleFactory.createFill(
+				filterFactory.literal(Color.LIGHT_GRAY), 
+				filterFactory.literal(0));
+		PolygonSymbolizer sym = styleFactory.createPolygonSymbolizer(stroke, fill, null); // null means default geometry
+		Rule rule = styleFactory.createRule();
+		FeatureTypeStyle fts = styleFactory.createFeatureTypeStyle(new Rule[]{rule});
+		Style style = styleFactory.createStyle();
+		style.featureTypeStyles().add(fts);
+		Logger.debug("created polygon style: " + style.toString());
+		return style;
+	}
+	
+	private Style createLineStyle()		// create a style to draw lines
+	{
+		Stroke stroke = styleFactory.createStroke(
+				filterFactory.literal(Color.BLACK), 
+				filterFactory.literal(1));
+		LineSymbolizer sym = styleFactory.createLineSymbolizer(stroke, null);	// null means default geometry
+		Rule rule = styleFactory.createRule();
+		rule.symbolizers().add(sym);
+		FeatureTypeStyle fts = styleFactory.createFeatureTypeStyle(new Rule[]{rule});
+		Style style = styleFactory.createStyle();
+		style.featureTypeStyles().add(fts);
+		Logger.debug("created line style: " + style.toString());		// JEB YES StyleImp[ name=Default Styler]
+		return style;
+	}
+	
+	private Style createPointStyle()	// create a style to draw points as circles
+	{
+		Graphic gr = styleFactory.createDefaultGraphic();
+		Mark mark = styleFactory.getCircleMark();
+		mark.setStroke(styleFactory.createStroke( 
+				filterFactory.literal(Color.BLACK), 
+				filterFactory.literal(1)));
+		mark.setFill(styleFactory.createFill(filterFactory.literal(Color.TRANSLUCENT)));
+		gr.graphicalSymbols().clear();gr.graphicalSymbols().add(mark);
+		gr.setSize(filterFactory.literal(5));
+		
+		PointSymbolizer sym = styleFactory.createPointSymbolizer(gr, null);	// null means default geometry
+		Rule rule = styleFactory.createRule();
+		rule.symbolizers().add(sym);
+		FeatureTypeStyle fts = styleFactory.createFeatureTypeStyle(new Rule[]{rule});
+		Style style = styleFactory.createStyle();
+		style.featureTypeStyles().add(fts);
+		Logger.debug("created point style: " + style.toString());
+		return style;
+	}
+	
+	public File getShapeFile()	// get shapefile (vFile)
+	{
+		return vFile;
+	}
+	
+	public String getShapePath()	// get string representing path for shapefile (shpPath)
+	{
+		return shpPath;
+	}
+	
+	public FeatureSource getFeatureSource()	// get FeatureSource associated with this map layer
+	{
+		return vFeatureSource;
+	}
+	
+	public Style getStyle()			// get Style
+	{
+		return vStyle;
+	}
+	
+	public void setStyle(Style aStyle)
+	{
+		vStyle = aStyle;			// set the style from another part of the program
+	}
+	
+	public Layer getLayer()			// get Layer based on FeatureSource and Style
+	{
+		if(vLayer == null)
+		{
+			vLayer = new FeatureLayer(vFeatureSource, vStyle);
+		}
+		return vLayer;
+	}
+	
+	public CoordinateReferenceSystem getCoordinateReferenceSystem()	// get CoordinateReferenceSystem
+	{
+		return vCRS;
+	}
+}
