736619b706198ef236100fc35eba7d5e417ec057
systemsgo <tahoward@nc.rr.com>
2016-02-12 00:37:41 -0500

    Remove debugging and test code (that caused float based files to fail)

--- verdi_data_loaders/src/anl/verdi/loaders/MPASDataset.java
+++ verdi_data_loaders/src/anl/verdi/loaders/MPASDataset.java
@@ -2,14 +2,10 @@ package anl.verdi.loaders;
 
 import java.io.IOException;
 import java.net.URL;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -17,7 +13,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SimpleTimeZone;
-import java.util.TreeSet;
 
 import org.apache.logging.log4j.LogManager;		// 2014
 import org.apache.logging.log4j.Logger;			// 2014 replacing System.out.println with logger messages
@@ -26,43 +21,15 @@ import org.apache.logging.log4j.Logger;			// 2014 replacing System.out.println w
 import org.unitsofmeasurement.unit.Unit;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-import javax.swing.*;
-
-import java.awt.image.*;
 import java.awt.*;
 
 import ucar.ma2.ArrayDouble;
 import ucar.ma2.ArrayInt;
 import ucar.ma2.DataType;
-//import simphony.util.messages.MessageCenter;
 import ucar.nc2.Attribute;
 import ucar.nc2.Dimension;
-import ucar.nc2.dataset.CoordinateAxis;
 import ucar.nc2.dataset.CoordinateAxis1D;
-import ucar.nc2.dataset.CoordinateAxis1DTime;
-import ucar.nc2.dataset.CoordinateSystem;
 import ucar.nc2.dataset.NetcdfDataset;
-import ucar.nc2.dataset.VariableEnhanced;
-import ucar.nc2.dt.GridCoordSystem;
-import ucar.nc2.dt.GridDatatype;
-import ucar.nc2.dt.grid.GridDataset;
-import ucar.unidata.geoloc.LatLonPoint;
-import ucar.unidata.geoloc.LatLonPointImpl;
-import ucar.unidata.geoloc.Projection;
-import ucar.unidata.geoloc.ProjectionPoint;
-import ucar.unidata.geoloc.ProjectionPointImpl;
 import anl.verdi.data.AbstractDataset;
 import anl.verdi.data.Axes;
 import anl.verdi.data.AxisType;
@@ -86,19 +53,6 @@ import anl.verdi.util.VUnits;
  */
 public class MPASDataset extends AbstractDataset {
 	static final Logger Logger = LogManager.getLogger(MPASDataset.class.getName());
-
-//	private static final MessageCenter msgCenter = MessageCenter.getMessageCenter(GridNetcdfDataset.class);
-	private static Map<ucar.nc2.constants.AxisType, AxisType> types = new HashMap<ucar.nc2.constants.AxisType, AxisType>();
-
-	/*static {
-		types.put(ucar.nc2.constants.AxisType.GeoX, AxisType.X_AXIS);
-		types.put(ucar.nc2.constants.AxisType.GeoY, AxisType.Y_AXIS);
-		types.put(ucar.nc2.constants.AxisType.Lon, AxisType.X_AXIS);
-		types.put(ucar.nc2.constants.AxisType.Lat, AxisType.Y_AXIS);
-		types.put(ucar.nc2.constants.AxisType.GeoZ, AxisType.LAYER);
-		types.put(ucar.nc2.constants.AxisType.Height, AxisType.LAYER);
-		types.put(ucar.nc2.constants.AxisType.Time, AxisType.TIME);
-	}*/
 	
 	public static final String VAR_AVG_CELL_DIAM = "verdi.avgCellDiam";
 
@@ -113,7 +67,7 @@ public class MPASDataset extends AbstractDataset {
 	int numCells;
 	int maxEdges = 0;
 
-	double[] legendLevels = null;
+	//double[] legendLevels = null;
 	
 	private static Set<String> hiddenVars = new HashSet<String>();
 	
@@ -122,25 +76,12 @@ public class MPASDataset extends AbstractDataset {
 	private class CellInfo { 
 		double[] latCoords;
 		double[] lonCoords;
-		int[] latTransformed;
-		int[] lonTransformed;
 		int cellId;
-		int colorIndex;
 		
 		private CellInfo(int id, int numVertices) {
 			latCoords = new double[numVertices];
 			lonCoords = new double[numVertices];
-			latTransformed = new int[numVertices];
-			lonTransformed = new int[numVertices];
 			cellId = id;
-		}
-		
-		private void transformCell(double factor, int imageWidth, int imageHeight) {
-			for (int i = 0; i < lonCoords.length; ++i) {
-				lonTransformed[i] = (int)Math.round((lonCoords[i] - lonMin) * factor);
-				latTransformed[i] = (int)Math.round((latCoords[i] - latMin) * factor);
-			}
-			colorIndex = indexOfObsValue((float) renderVariable.get(0, cellId), legendLevels);
 		}
 		
 		public CellInfo clone() {
@@ -153,9 +94,6 @@ public class MPASDataset extends AbstractDataset {
 	
 	private CellInfo[] cellsToRender = null;
 	private ArrayList<CellInfo> splitCells = null;
-	int previousWindowWidth = 0;
-	int sscaledWidth = 0;
-	int sscaledHeight = 0;
 	
 	static {
 		renderVarList.add("verticesOnCell");
@@ -185,7 +123,6 @@ public class MPASDataset extends AbstractDataset {
 		hiddenVars.add("meshDensity");
 		hiddenVars.add("zz");
 	}
-	//TODO - layers: nVertLevelsP1, nSoilLevels
 	
 	ucar.ma2.Array cellVertices = null;
 	ucar.ma2.Array latCell = null;
@@ -247,7 +184,6 @@ public class MPASDataset extends AbstractDataset {
 		long start = System.currentTimeMillis();
 		this.dataset = netcdfDataset;
 		initVariables();
-		initLegend();
 		try {
 			initAxes();
 		} catch (IOException e) {
@@ -522,11 +458,9 @@ public class MPASDataset extends AbstractDataset {
 			
 			if (renderVarList.contains(name) || name.startsWith("verdi.")) {
 				this.renderVars.put(name, var);
-				//TODO - what should units be?
 				this.verdiRenderVars.add(new DefaultVariable(name, name, VUnits.MISSING_UNIT, this));
 			}
 			else if (!hiddenVars.contains(name) &&
-					//var.getDimensionsString().indexOf("Time") != -1 &&
 					var.getDimensionsString().indexOf("nCells") != -1
 					) 
 				{
@@ -553,185 +487,8 @@ public class MPASDataset extends AbstractDataset {
 		return att.getStringValue();
 	}
 	
-	   private static final Stroke STROKE = new BasicStroke(12f,
-	            BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
-	   
-	   public static void main(String[] args) {
-		   MPASSwingTest.main(args);
-	   }
-	public static void mainn(String[] args) {
-	      java.awt.EventQueue.invokeLater(new Runnable() {
-	          public void run() {
-		String loc = "file:///home/tahoward/allstate/history.2015-08-12_00.00.00.nc";
-		try {
-			NetcdfDataset ds = NetcdfDataset.openDataset(loc);
-			MPASDataset mpasDs = new MPASDataset(new URL(loc), ds);
-			List<ucar.nc2.dataset.CoordinateAxis> list = ds.getCoordinateAxes();
-			System.out.println("Got " + list.size() + " axes");
-			Dimension dimt = ds.findDimension("Time");
-			System.out.println("Got dim " + dimt);
-			System.out.println("Dim " + dimt.getShortName() + " " + dimt.getLength());
-
-			ucar.nc2.Variable snow = null;
-			List<ucar.nc2.Variable> vars = ds.getVariables();
-			for (ucar.nc2.Variable var : vars) {
-				//System.out.println("Got var " + var.getNameAndDimensions());
-				if (var.getShortName().equals("snow"))
-					snow = var;
-			}
-			
-			
-			System.out.println("Snow size " + snow.getSize() + " shape " + Arrays.toString(snow.getShape()));
-			System.out.println("Attr units: " + snow.getUnitsString() + " arr " + snow.read().getClass());
-			System.out.println("With " + ds.getCoordinateAxes().size() + " axes");
-			ucar.ma2.ArrayDouble.D2 arr = (ucar.ma2.ArrayDouble.D2)snow.read();
-			System.out.println("Shape " + Arrays.toString(arr.getShape()));
-
-			
-
-
-			
-
-		  			JFrame frame = new JFrame("HelloWorldSwing");
-			        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-			        //Add the ubiquitous "Hello World" label.
-			        //JLabel label = new JLabel("Hello World");
-			        frame.setSize(1024, 768);
-			       // frame.getContentPane().add(label);
-
-			        //Display the window.
-			        frame.pack();
-			        frame.setVisible(true);
-			        frame.setSize(1024, 768);
-			        
-			        /*java.awt.Graphics frameGraphics = frame.getContentPane().getGraphics();
-			        frameGraphics.setColor(Color.RED);
-			        frameGraphics.setPaintMode();
-			        //frameGraphics.drawImage(img, 0, 0, Color.WHITE, null);
-			        frameGraphics.fillRect(0,  0,  400,  400);*/
-
-
-			
-			ds.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	          }
-	       });
-	}
-	
 	Palette defaultPalette =  new PavePaletteCreator().createPalettes(8).get(0);
 	Color[] legendColors = defaultPalette.getColors();
-	ArrayDouble.D2 renderVariable = null;
-	double varMin = Double.MAX_VALUE;
-	double varMax = 0;
-
-	private void initLegend() {
-		//String renderVarName = "surface_pressure";
-		String renderVarName = "snowh"; // 0 to 0.01452, little picture
-		renderVarName = "xice"; //0 to 1, only red
-		renderVarName = "vegfra"; //0 to 99, decent pic
-		renderVarName = "glw";  //only 0
-		renderVarName = "gsw";  //only 0
-		renderVarName = "surface_pressure";
-		
-		renderVariable = null;
-		List<ucar.nc2.Variable> vars = dataset.getVariables();
-		for (ucar.nc2.Variable var : vars) {
-			//System.out.println("Got var " + var.getNameAndDimensions());
-			if (var.getShortName().equals(renderVarName))
-				try {
-					renderVariable = (ArrayDouble.D2)var.read();
-				} catch (IOException e) {
-					e.printStackTrace();
-					return;
-				}
-		}
-		
-		for (int i = 0; i < numCells; ++i) {
-			double v = renderVariable.get(0, i);
-			if (v < varMin)
-				varMin = v;
-			if (v > varMax)
-				varMax = v;
-		}
-		
-		int count = legendColors.length + 1;
-		final double delta = (varMax - varMin) / (count - 1);
-		legendLevels = new double[count];
-		for (int level = 0; level < count; ++level) {
-			legendLevels[level] = varMin + level * delta;
-		}
-		previousWindowWidth = 0;
-		
-	}
-	
-	private static int indexOfObsValue(float value, final double[] values) {
-		if (new Float(value).toString().equals("NaN"))
-			return -1;
-		
-		if (value <= DataUtilities.BADVAL3 || value <= DataUtilities.AMISS3) 	// 2014 changed AMISS3 comparison from == to <=
-			return -1;
-
-		final int count = values.length;
-
-		if (values[0] == values[values.length - 1])
-			return 0;
-
-		for (int index = 1; index < count; index++) {
-			if (values[index] > value)
-				return index - 1;
-		}
-
-		return count - 2;
-	}
-
-	public void renderCells(Graphics gr, int windowWidth, int windowHeight) {
-		
-		if (previousWindowWidth != windowWidth)
-			transformCells(gr, windowWidth, windowHeight);
-		
-		long renderStart = System.currentTimeMillis();
-		
-		//int imageWidth = 8192;
-		
-		/*
-		BufferedImage img = new java.awt.image.BufferedImage(imageWidth, imageHeight, java.awt.image.BufferedImage.TYPE_3BYTE_BGR);
-        java.awt.Graphics2D g = img.createGraphics();
-        */
-
-		for (int i = 0; i < numCells; ++i) { //for each cell
-			if (cellsToRender[i].colorIndex == -1)
-				continue;
-			gr.setColor(legendColors[cellsToRender[i].colorIndex]);
-			gr.fillPolygon(cellsToRender[i].lonTransformed, cellsToRender[i].latTransformed, cellsToRender[i].lonTransformed.length);
-		}
-		for (int i = 0; i < splitCells.size(); ++i) {
-			if (splitCells.get(i).colorIndex == -1)
-				continue;
-			gr.setColor(legendColors[splitCells.get(i).colorIndex]);
-			gr.fillPolygon(splitCells.get(i).lonTransformed, splitCells.get(i).latTransformed, splitCells.get(i).lonTransformed.length);
-		}
-		long renderTime = System.currentTimeMillis() - renderStart;
-		System.out.println("Finished drawing image " + new Date() + " image in " + renderTime + "ms  window " + windowWidth + "x" + windowHeight);
-		System.out.println("Var min " + varMin + " max " + varMax);
-		/*
-		java.io.File outputFile = new java.io.File("/tmp/mpasout.png");
-		try {
-			boolean res = javax.imageio.ImageIO.write(img, "png", outputFile);
-			System.out.println("Image written: " + res + " " + new Date());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		gr.drawImage(img.getScaledInstance(windowWidth, -1, Image.SCALE_FAST), 0, 0, null);
-		*/
-		System.out.println("Image drawn to screen " + new Date());
-		
-	}
 	
 	private void setCalendar(String timeStr, Calendar cal) {
 		String[] arr = timeStr.split("[-_:.]");
@@ -747,10 +504,7 @@ public class MPASDataset extends AbstractDataset {
 		if (arr.length > 6)
 			cal.set(Calendar.MILLISECOND, Integer.parseInt(arr[6]));	
 	}
-	
-	private static final SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
-	private static final SimpleDateFormat TIME_DURATION_FORMAT = new SimpleDateFormat("D_HH:mm:ss");
-	
+		
 	private CoordAxis makeTimeCoordAxis(String timeName) {
 		//TODO - come up with exception handling strategy
 		String start_str = findAttributeStr("config_start_time");
@@ -835,20 +589,13 @@ public class MPASDataset extends AbstractDataset {
 			int numVertices = indexToVertexId.getShape()[0];
 			for (int i = 0; i < numVertices; ++i)
 				vertexPositionMap.put(indexToVertexId.getInt(i), i);
-
-			
-			//Set<Double> latCoords = new TreeSet<Double>();
-			//Set<Double> lonCoords = new TreeSet<Double>();
 			
 			int[] vertexShape = vertexList.getShape();
 
 			cellsToRender = new CellInfo[vertexShape[0]];
 			splitCells = new ArrayList<CellInfo>();
-			//get average cell diameter
-			//get lat max min
-			//get lon max min
-			//num steps = ceil(lat range / diam)
-			//step size = range / numSteps
+
+		
 			long cellDiamCount = 0;
 			double cellDiamSum = 0;
 			avgCellDiam = 0;
@@ -965,25 +712,6 @@ public class MPASDataset extends AbstractDataset {
 		if (vertList.length > 0)
 			axisList.add(new CSVCoordAxis(vertList, layerName, layerName, AxisType.LAYER));
 	}
-	
-	private void transformCells(Graphics gr, int windowWidth, int windowHeight) {
-		long start = System.currentTimeMillis();
-		double factor = windowWidth / dataWidth;
-		previousWindowWidth = windowWidth;
-
-		gr.setColor(Color.BLACK);
-		gr.fillRect(0,  0,  windowWidth, windowHeight);
-		int imageWidth = windowWidth;
-		int imageHeight = (int)Math.round(imageWidth * dataHeight / dataWidth);
-		for (int i = 0; i < cellsToRender.length; ++i) {
-			cellsToRender[i].transformCell(factor, imageWidth, imageHeight);		
-		}
-		for (int i = 0; i < splitCells.size(); ++i) {
-			splitCells.get(i).transformCell(factor, imageWidth, imageHeight);
-		}
-		System.out.println("Scaled cells in " + (System.currentTimeMillis() - start) + "ms");
-	}
-
 
 	/**
 	 * Gets the coordindate Axes for this Dataset.
